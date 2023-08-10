7c90155035ff2ddd589e1c15a5208ad50e5645aa
Jacob Di Lorenzo <jadilorenzo@icloud.com>
2023-08-10 12:07:26 -0400

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :documents, only: [:index, :update, :show]
   get '/documents/:id/json/body', to: 'documents#json_document'
   get '/documents/:id/json/title', to: 'documents#json_title'
-
+  
+  resources :users
+  get  '/signup',  to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
